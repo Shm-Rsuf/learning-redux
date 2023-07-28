234de2f9f3e6c4c0dfec9eb2f166c278c54cbc9c
shm_usuf <shmusuf@gmail.com>
2023-07-28 13:34:26 +0600

    index.js added

--- module-4.1/index.js
+++ module-4.1/index.js
@@ -1,1 +1,33 @@
-console.log("hello");
+const { createStore } = require("redux");
+// initial state
+const initialState = {
+  todos: [],
+};
+
+//reducer
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "todos/addedTodo":
+      return {
+        ...state,
+        todos: [...state.todos, { title: action.payload }],
+      };
+
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(todosReducer);
+
+//subscription to store
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+//action dispatch
+store.dispatch({
+  type: "todos/addedTodo",
+  payload: "learn redux",
+});
