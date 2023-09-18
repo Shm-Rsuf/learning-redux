0db69bdfd35df2134efe491cca5d48c4b7883ec2
shm_usuf <shmusuf@gmail.com>
2023-09-18 10:43:35 +0600

    modlue 5.3 rtk added

---
+++ module-5/rtk/index.js
@@ -0,0 +1,25 @@
+const store = require("./app/store");
+const { counterActions } = require("./features/counter/counterSlice");
+const {
+  dynamicCounterActions,
+} = require("./features/dynamicCounter/dynamicCounterSlice");
+
+// console.log(`Initial state: ${JSON.stringify(store.getState())}`);
+
+// subscribe to state changes
+store.subscribe(() => {
+  // console.log(store.getState());
+});
+
+// disptach actions
+// store.dispatch(counterActions.increment());
+
+// store.dispatch(counterActions.increment());
+
+// store.dispatch(counterActions.decrement());
+
+store.dispatch(dynamicCounterActions.increment(2));
+
+store.dispatch(dynamicCounterActions.increment(3));
+
+store.dispatch(dynamicCounterActions.decrement(5));
