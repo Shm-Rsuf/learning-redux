4b2ed3eb65cdcea47c7f0c3ab4ec4b5283d26a41
shm rsuf <shmusuf@gmail.com>
2024-01-01 09:12:32 +0600

    reactive ui and reusability added

---
+++ module-1/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className='p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
+      <div className='text-2xl font-semibold'>{count}</div>
+      <div className='flex space-x-3'>
+        <button
+          className='bg-indigo-400 text-white px-3 py-2 rounded shadow'
+          onClick={() => setCount((previous) => previous + 1)}
+        >
+          Increment
+        </button>
+        <button
+          className='bg-red-400 text-white px-3 py-2 rounded shadow'
+          onClick={() => setCount((previous) => previous - 1)}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
