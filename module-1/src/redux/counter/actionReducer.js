37e01867671fdba53cf5e9dbf76a5501d920ea67
shm rsuf <shmusuf@gmail.com>
2024-01-07 22:51:57 +0600

    redux folder added

---
+++ module-1/src/redux/counter/actionReducer.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { DECREMENT, INCREMENT } from "./actionTypes";
+import { initialState } from "./initialState";
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
---
+++ module-1/src/redux/counter/actionTypes.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars */
+export const INCREMENT = "counter/increment";
+export const DECREMENT = "counter/decrement";
